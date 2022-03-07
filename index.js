f32f9d79b4e7c81bc725cf74b4e39cfdf884111d
Ruhul <ruhul20@gmail.com>
2022-03-07 12:40:50 +0600

    npm update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const http = require('http');
 
-const requestListener = function(req, res) {
-    res.writeHead(200);
-    res.end('Hello, World!');
-}
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
