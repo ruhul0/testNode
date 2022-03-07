0df0c9ff2e321a9a7a0cc279d49931f17dd3eee2
Ruhul <ruhul20@gmail.com>
2022-03-07 13:21:40 +0600

    updated

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 4000;
 
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 app.get('/data', function(req, res) {
     console.log("test");
 });
+
+
 
 app.listen(4000);
